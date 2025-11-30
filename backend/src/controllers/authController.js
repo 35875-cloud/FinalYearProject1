63dbbae4e6ba0d5771ab4c791886213101426bea
Talha-35875 <35875@students.riphah.edu.pk>
2025-11-30 10:48:44 +0000

    save final change

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 const nacl = require("tweetnacl"); 
 const CryptoJS = require("crypto-js");
 
-// ---------------------------------------------------
+// /---------------------------------------------------
 // REGISTER CITIZEN
 // ---------------------------------------------------
 exports.registerCitizen = async (req, res) => {
